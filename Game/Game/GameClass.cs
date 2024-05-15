901fc4d1f3a6fbad2694a7a04141333b16d0fd11
RomanEger <egerromaan@gmail.com>
2024-05-15 22:03:45 +0600

    final

---
+++ Game/Game/GameClass.cs
@@ -0,0 +1,38 @@
+﻿namespace Game;
+
+public class GameClass
+{
+    public bool Game(int[] candy)
+    {
+        int maryPoints = 0;
+        
+        int nikolayPoints = 0;
+        
+        var candyList = candy.ToList();
+
+        bool isMaryStep = true; 
+        
+        while (candyList.Count > 0)
+        {
+            if (candyList.First() > candyList.Last())
+            {
+                if (isMaryStep)
+                    maryPoints += candyList.First();
+                else 
+                    nikolayPoints += candyList.First();
+                candyList.Remove(candyList.First());
+            }
+            else
+            {
+                if (isMaryStep)
+                    maryPoints += candyList.Last();
+                else 
+                    nikolayPoints += candyList.Last();
+                candyList.Remove(candyList.Last());
+            }
+            isMaryStep = !isMaryStep;
+        }
+
+        return maryPoints > nikolayPoints;
+    }
+}
--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Game;
 
-Console.WriteLine("Hello, World!");
+var list = new List<int>();
+
+Console.WriteLine("Введите массив через пробел");
+
+var stringArray = Console.ReadLine().Split(' ');
+
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if(int.TryParse(stringArray[i], out int x))
+        list.Add(x);
+}
+var game = new GameClass();
+
+var gameResult = game.Game(list.ToArray());
+
+Console.WriteLine(gameResult);
